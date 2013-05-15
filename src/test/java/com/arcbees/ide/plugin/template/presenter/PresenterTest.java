cdcd2fbad2d7e4246947c70c33cc4787a37c8bfc
Brandon Donnelson <branflake2267@gmail.com>
2013-05-14 18:46:31 -0700

    Spaces for test.

--- src/test/java/com/arcbees/ide/plugin/template/presenter/PresenterTest.java
+++ src/test/java/com/arcbees/ide/plugin/template/presenter/PresenterTest.java
@@ -9,122 +9,122 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PresenterTest {
-	private VelocityEngine velocityEngine;
+    private VelocityEngine velocityEngine;
 
-	@Before
-	public void testSetup() {
-		velocityEngine = new VelocityEngine();
-		velocityEngine.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH,
-				"./src/main/resources/com/arcbees/ide/plugin/template/presenter/");
-		velocityEngine.init();
-	}
+    @Before
+    public void testSetup() {
+        velocityEngine = new VelocityEngine();
+        velocityEngine.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH,
+                "./src/main/resources/com/arcbees/ide/plugin/template/presenter/");
+        velocityEngine.init();
+    }
 
-	@Test
-	public void testModule() {
-		Template t = velocityEngine.getTemplate("__name__Module.java");
+    @Test
+    public void testModule() {
+        Template t = velocityEngine.getTemplate("__name__Module.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("package", "com.arcbees.project.client.application");
-		context.put("name", "ApplicationTest");
+        VelocityContext context = new VelocityContext();
+        context.put("package", "com.arcbees.project.client.application");
+        context.put("name", "ApplicationTest");
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testPresenter() {
-		Template t = velocityEngine.getTemplate("__name__Presenter.java");
+    @Test
+    public void testPresenter() {
+        Template t = velocityEngine.getTemplate("__name__Presenter.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("package", "com.arcbees.project.client.application");
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", false);
+        VelocityContext context = new VelocityContext();
+        context.put("package", "com.arcbees.project.client.application");
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", false);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testPresenterWithUiHandlers() {
-		Template t = velocityEngine.getTemplate("__name__Presenter.java");
+    @Test
+    public void testPresenterWithUiHandlers() {
+        Template t = velocityEngine.getTemplate("__name__Presenter.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("package", "com.arcbees.project.client.application");
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", true);
+        VelocityContext context = new VelocityContext();
+        context.put("package", "com.arcbees.project.client.application");
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", true);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testUiHandlers() {
-		Template t = velocityEngine.getTemplate("__name__UiHandlers.java");
+    @Test
+    public void testUiHandlers() {
+        Template t = velocityEngine.getTemplate("__name__UiHandlers.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("package", "com.arcbees.project.client.application");
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", false);
+        VelocityContext context = new VelocityContext();
+        context.put("package", "com.arcbees.project.client.application");
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", false);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testView() {
-		Template t = velocityEngine.getTemplate("__name__View.java");
+    @Test
+    public void testView() {
+        Template t = velocityEngine.getTemplate("__name__View.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", false);
+        VelocityContext context = new VelocityContext();
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", false);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testViewWithUiHandlers() {
-		Template t = velocityEngine.getTemplate("__name__View.java");
+    @Test
+    public void testViewWithUiHandlers() {
+        Template t = velocityEngine.getTemplate("__name__View.java");
 
-		VelocityContext context = new VelocityContext();
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", true);
+        VelocityContext context = new VelocityContext();
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", true);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 
-	@Test
-	public void testViewUi() {
-		Template t = velocityEngine.getTemplate("__name__View.ui.xml");
+    @Test
+    public void testViewUi() {
+        Template t = velocityEngine.getTemplate("__name__View.ui.xml");
 
-		VelocityContext context = new VelocityContext();
-		context.put("package", "com.arcbees.project.client.application");
-		context.put("name", "ApplicationTest");
-		context.put("uihandlers", false);
+        VelocityContext context = new VelocityContext();
+        context.put("package", "com.arcbees.project.client.application");
+        context.put("name", "ApplicationTest");
+        context.put("uihandlers", false);
 
-		StringWriter writer = new StringWriter();
-		t.merge(context, writer);
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
 
-		// TODO
-		System.out.println(writer.toString());
-	}
+        // TODO
+        System.out.println(writer.toString());
+    }
 }
